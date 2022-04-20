60cdabbef919ce5d5fb57f8906ce38b1c5df8ed6
louis.luo <louis0420tw@gmail.com>
2022-04-20 22:51:44 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+const { watch, src, series, dest, parallel } = require("gulp");
+const browserSync = require("browser-sync").create();
+const bundlePath = "./dist";
+const del = require("del");
+const gulpSass = require('gulp-sass')(require('sass'));
+const rename = require("gulp-rename");
+const package = require("./package.json");
+
+// Watch HTML Files
+function html() {
+  return src("./pages/**.html")
+    .pipe(dest(bundlePath))
+    .pipe(browserSync.stream());
+}
+
+// Compile and Minify Img
+function images() {
+  return src("./assets/images/**").pipe(dest(`${bundlePath}/images`));
+}
+// Compile Svg Icons
+function icons() {
+  return src("./assets/icons/**").pipe(dest(`${bundlePath}/icons`));
+}
+
+function styles() {
+  return src("./assets/styles/*.css").pipe(dest(`${bundlePath}/styles`));
+}
+
+// Compile SCSS/SASS to CSS
+function scss() {
+  return src("./scss/main.scss")
+    .pipe(gulpSass())
+    .on("error", gulpSass.logError)
+    .pipe(rename(`${package.name}.css`))
+    .pipe(dest(`${bundlePath}/styles`))
+    .pipe(browserSync.stream());
+}
+
+// Clean Up (deleted `dist` folder)
+function clean() {
+  return del([bundlePath]);
+}
+
+// Web Server / Live Reload
+function server() {
+  browserSync.init({
+    server: {
+      baseDir: bundlePath,
+    },
+    port: 4000,
+    // serveStatic: ['./'],
+  });
+  watch("./pages/**/*.html", html);
+  watch("./scss/**/*.scss", scss);
+  watch("./assets/images/*.(jpe?g|png)", images);
+  watch("./assets/icons/*.svg", icons);
+  browserSync.reload();
+}
+
+exports.clean = clean;
+exports.build = series(
+  clean,
+  parallel(html, scss, styles, images, icons)
+);
+exports.start = series(
+  clean,
+  parallel(html, scss, styles, images, icons, server)
+);
